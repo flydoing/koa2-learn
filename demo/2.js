58141f863216776e1c04675297112eabe43491eb
Jam <guo3jin3cai@gmail.com>
2018-05-04 08:35:33 +0800

    template/async

--- demo/2.js
+++ demo/2.js
@@ -5,6 +5,12 @@ const app = new Koa()
 // 静态资源
 const path = require('path')
 const static = require('koa-static')
+
+// template资源
+const fs = require('fs')
+// 异步操作
+const fsPromised = require('fs.promised')
+
 // 静态资源目录对于相对入口文件index.js的路径
 const staticPath = './static'
 app.use(static(path.join( __dirname,  staticPath)))
@@ -22,7 +28,19 @@ const about = ctx => {
   ctx.response.body = '关于 <a href="/">Index Page</a>'
 }
 
+const template = ctx => {
+  ctx.response.type = 'html'
+  ctx.response.body = fs.createReadStream('./template/text.html')
+}
+
+const asyncReadFile = async function (ctx, next) {
+// const asyncReadFile = ctx => {
+  ctx.response.type = 'html'
+  ctx.response.body = await fsPromised.readFile('./template/text.html', 'utf8')
+}
+
 app.use(route.get('/', main))
 app.use(route.get('/about', about))
-
+app.use(route.get('/template', template))
+app.use(route.get('/asyncReadFile', asyncReadFile))
 app.listen(3000)
