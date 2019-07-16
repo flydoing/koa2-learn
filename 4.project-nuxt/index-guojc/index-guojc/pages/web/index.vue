4b44be61068293907a19001aebd778f5cadf4801
Jam <guo3jin3cai@gmail.com>
2019-07-16 13:03:11 +0800

    add project nuxt

---
+++ 4.project-nuxt/index-guojc/index-guojc/pages/web/index.vue
@@ -0,0 +1,141 @@
+<template>
+  <div class="vx-pages-index">
+    <!-- <ul id="menu">
+      <li data-menuanchor="page1" class="active">
+        <a href="#page1">Section 1</a>
+      </li>
+      <li data-menuanchor="page2" class="">
+        <a href="#page2">Section 2</a>
+      </li>
+      <li data-menuanchor="page3" class="">
+        <a href="#page3">Section 3</a>
+      </li>
+    </ul> -->
+    <div class="vx-header">
+      <div class="header">
+        <div class="header-logo">
+          V校 让学校更智慧
+        </div>
+        <div id="header-menu" class="header-menu">
+          <a href="#page1">平台</a>
+          <a href="#page2">应用软件</a>
+          <a href="#page3">只能硬件</a>
+        </div>
+      </div>
+    </div>
+    <no-ssr>
+      <full-page id="fullpage" :options="options">
+        <div class="section">
+          <h3>Section 1</h3>
+        </div>
+        <div class="section">
+          <div class="slide">
+            <h3>Slide 2.1</h3>
+          </div>
+          <div class="slide">
+            <h3>Slide 2.2</h3>
+          </div>
+          <div class="slide">
+            <h3>Slide 2.3</h3>
+          </div>
+        </div>
+        <div class="section">
+          <h3>Section 3</h3>
+        </div>
+      </full-page>
+    </no-ssr>
+  </div>
+</template>
+
+<script>
+// import Logo from '~/components/Logo.vue'
+// import Vue from 'vue'
+// import VueFullPage from 'vue-fullpage.js'
+// if (process.browser) {
+//   window.VueFullpage = VueFullPage
+//   Vue.use(VueFullPage)
+// }
+// Vue.use(VueFullPage)
+
+export default {
+  data() {
+    return {
+      options: {
+        licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+        menu: '#header-menu',
+        scrollBar: false,
+        navigation: true,
+        controlArrows: true,
+        slidesNavigation: true,
+        anchors: ['page1', 'page2', 'page3'],
+        sectionsColor: [
+          '#41b883',
+          '#ff5f45',
+          '#0798ec',
+          '#fec401',
+          '#1bcee6',
+          '#ee1a59',
+          '#2c3e4f',
+          '#ba5be9',
+          '#b4b8ab'
+        ]
+      }
+    }
+  }
+  // components: {
+  //   Logo
+  // }
+}
+</script>
+
+<style lang="scss">
+.vx-pages-index {
+  background: yellowgreen;
+  #menu {
+    position: fixed;
+    top: 20px;
+    right: 20px;
+    z-index: 70;
+    -webkit-font-smoothing: antialiased;
+    -moz-font-smoothing: antialiased;
+    letter-spacing: 1px;
+    font-size: 1.1em;
+  }
+  #menu li {
+    display: inline-block;
+    margin: 10px 0;
+    position: relative;
+  }
+  #menu a {
+    color: #fff;
+    padding: 0 1.1em 1.1em 1.1em;
+  }
+  #menu .active a:after {
+    content: '';
+    margin: 0 1.1em 0 1.1em;
+    height: 2px;
+    background: #fff;
+    display: block;
+    position: absolute;
+    bottom: -6px;
+    left: 0;
+    right: 0;
+    display: block;
+  }
+}
+.vx-header {
+  position: fixed;
+  z-index: 10;
+  top: 0;
+  width: 100%;
+  height: 80px;
+  margin: 0 auto;
+  background: #fff;
+  -webkit-font-smoothing: antialiased;
+  -moz-font-smoothing: antialiased;
+  .header {
+    width: 1180px;
+    background: yellow;
+  }
+}
+</style>
---
+++ 4.project-nuxt/index-guojc/index.md
@@ -0,0 +1,43 @@
+init:
+
+1. install
+
+``` bash
+$ npx create-nuxt-app index-guojc
+npx: 379 安装成功，用时 153.483 秒
+create-nuxt-app v2.8.0
+✨  Generating Nuxt.js project in /Users/guojc/Documents/dev/github/koa2-learn/4.project-nuxt/index-guojc/index-guojc
+? Project name index-guojc
+? Project description website for index
+? Author name guojc
+? Choose the package manager Npm
+? Choose UI framework None
+? Choose custom server framework None (Recommended)
+? Choose Nuxt.js modules (Press <space> to select, <a> to toggle all, <i> to invert selection)
+? Choose linting tools (Press <space> to select, <a> to toggle all, <i> to invert selection)ESLint, Prettier
+? Choose test framework None
+? Choose rendering mode Universal (SSR)
+npm WARN deprecated flatten@1.0.2: I wrote this module a very long time ago; you should use something else.
+
+⠋ Installing packages with npm
+
+......
+🎉  Successfully created project index-guojc
+
+  To get started:
+
+	cd index-guojc
+	npm run dev
+
+  To build & start for production:
+
+	cd index-guojc
+	npm run build
+	npm run start
+
+```
+
+2. install vue-fullpage.js
+$ npm install --save vue-fullpage.js
+$ npm install --save nuxt-fullpage.js
+
