00136fdaf8046a350faa9867a51a9a8fcec0dffa
guojincai <guojc@mashang.cn>
2019-08-29 23:37:48 +0800

    logger

--- 2.learn-project/app_routes.js
+++ 2.learn-project/app_routes.js
@@ -3,10 +3,18 @@ const config = require('./config')
 const router = require('./routes')
 const handleError = require('./middleware/handleError')
 
+// logger
+const logger = async (ctx, next) => {
+  console.dir(`logger>>>>>>>>`)
+  console.dir(`${new Date()} ${ctx.request.method} ${ctx.request.url}`)
+  next()
+}
+
 const app = new Koa()
 // 配置：config
 app.config = config
 // 中间件：middleware
+app.use(logger)
 app.use(handleError)
 // 路由：router
 app.use(router.routes()).use(router.allowedMethods())
